2a60857f12a3e3ab01ac93c22393114cb4e20e33
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-02-22 13:13:53 +0400

    to show

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
 import csv
+import math
+
 import numpy as n
 import random as r
 
-#numbers = n.random.normal(500,15,(500,4))
-numbers = [[r.randint(40,50) for i in range(500)],
-           [r.randint(500,520) for i in range(500)],
-           [r.randint(400,440) for i in range(500)],
-           [r.randint(20,25) for i in range(500)]]
+numbers = [n.random.normal(100,20,500),
+           n.random.normal(40,10,500),
+           n.random.normal(150,50,500),
+           n.random.normal(90,5,500)]
+
+for i in range(len(numbers)):
+    for j in range(len(numbers[i])):
+        numbers[i][j] = round(numbers[i][j],1)
 
 numbers = n.transpose(numbers)
+for i in range(r.randint(0,100)):
+    numbers[r.randint(1,499)][r.randint(1,3)] = n.nan
 
 with open("test.csv", "w" , newline='') as f:
     writer = csv.writer(f)
     writer.writerow(["Заголовок 1","Заголовок 2","Заголовок 3","Заголовок 4"])
     writer.writerows(numbers)
+
+def MaxInList():
+    max = numbers[0][0]
+    for i in range(len(numbers)):
+        for j in range(len(numbers[i])):
+            if numbers[i][j] > max:
+                max = numbers[i][j]
+    return max
+
+def MinInList():
+    min = numbers[0][0]
+    for i in range(len(numbers)):
+        for j in range(len(numbers[i])):
+            if numbers[i][j] < min:
+                min = numbers[i][j]
+    return min
+
+def AverageinList():
+    count = 0
+    sum = 0
+    for i in range(len(numbers)):
+        for j in range(len(numbers[i])):
+            if math.isnan(numbers[i][j]) != True :
+                sum += numbers[i][j]
+                count += 1
+    return sum/count
+
+print(MaxInList(),MinInList(),AverageinList())
