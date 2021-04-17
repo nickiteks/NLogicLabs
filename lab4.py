dd2fdbff8925be4e7a51e25fa24bb4cba6e7353e
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-04-17 17:56:18 +0400

    4 fix

--- lab4.py
+++ lab4.py
@@ -1,8 +1,17 @@
+import math
 from bloom import BloomFilter
 import docx2txt
 import time
 
-a = [BloomFilter(20000, 2)]
+
+m1 = int((-5000*math.log(0.1))/pow(math.log(2), 2))
+m2 = int((-10000*math.log(0.5))/pow(math.log(2), 2))
+
+k1 = int((m1/5000) * math.log(2))
+k2 = int((m1/10000) * math.log(2))
+
+
+a = [BloomFilter(20000, 2), BloomFilter(1000, 4), BloomFilter(2000, 2), BloomFilter(5000, 5)]
 
 startTime = time.perf_counter_ns()
 text = docx2txt.process("gg.docx")
@@ -19,4 +28,4 @@ for bl in a:
     startTime = time.perf_counter_ns()
     print(bl.findWord("роза"))
     print()
-    print(f'Время выполнения: {(time.perf_counter_ns() - startTime)}')
+    print(f'Время выполнения: {(time.perf_counter_ns() - startTime)}\n')
