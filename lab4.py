96296edc925ee52f6ac735a18d6aae740973a2da
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-04-17 21:06:56 +0400

    4 fix

--- lab4.py
+++ lab4.py
@@ -3,20 +3,20 @@ from bloom import BloomFilter
 import docx2txt
 import time
 
+m1 = int((-5000 * math.log(0.1)) / pow(math.log(2), 2))
+m2 = int((-10000 * math.log(0.5)) / pow(math.log(2), 2))
 
-m1 = int((-5000*math.log(0.1))/pow(math.log(2), 2))
-m2 = int((-10000*math.log(0.5))/pow(math.log(2), 2))
+k1 = int((m1 / 5000) * math.log(2))
+k2 = int((m1 / 10000) * math.log(2))
 
-k1 = int((m1/5000) * math.log(2))
-k2 = int((m1/10000) * math.log(2))
-
-
-a = [BloomFilter(20000, 2), BloomFilter(1000, 4), BloomFilter(2000, 2), BloomFilter(5000, 5)]
+print(m1, m2, k1, k2)
+a = [BloomFilter(m1, k1), BloomFilter(m2, k2)]
 
 startTime = time.perf_counter_ns()
 text = docx2txt.process("gg.docx")
 for word in text.split():
     a[0].addWord(word)
+    a[1].addWord(word)
 print(f'Время выполнения add: {(time.perf_counter_ns() - startTime)}')
 
 startTime = time.perf_counter_ns()
